03d050dd53f7f57a575dbae2ac1ef9ad640122e2
a20sarle <a20sarle.student@his.se>
2021-04-06 12:20:07 +0200

    Created a private member variable called `myWebView` of the type `WebView`

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -13,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
 
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
